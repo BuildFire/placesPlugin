a49956fba1a9f1d99b02424747975abe6cd8214c
vineetasharma <vineetasharma18791@gmail.com>
2015-09-28 13:52:10 +0530

    BF-030, BF-031, BF-32 : Initial data fetch and add map

--- widget/controllers/widget.item.controller.js
+++ widget/controllers/widget.item.controller.js
@@ -1,11 +1,45 @@
 (function (angular, window) {
     angular
         .module('placesWidget')
-        .controller('WidgetItemCtrl', ['$scope', 'COLLECTIONS', 'DB', function ($scope, COLLECTIONS, DB) {
+        .controller('WidgetItemCtrl', ['$scope', 'COLLECTIONS', 'DB', '$routeParams', 'Buildfire', function ($scope, COLLECTIONS, DB, $routeParams, Buildfire) {
             var WidgetItem = this;
             WidgetItem.placeInfo = null;
             console.log('WidgetItemCtrl called');
             var PlaceInfo = new DB(COLLECTIONS.PlaceInfo);
+            var Items = new DB(COLLECTIONS.Items);
+            if ($routeParams.itemId) {
+                Items.getById($routeParams.itemId).then(
+                    function (result) {
+                        if (result && result.data)
+                            WidgetItem.item = result;
+                        WidgetItem.coordinates = [result.data.lat, result.data.lng];
+                    },
+                    function (err) {
+                        console.error('Error while getting item-', err);
+                    }
+                );
+            }
+            else {
+                WidgetItem.item = {
+                    data: {
+                        listImage: "",
+                        itemTitle: "",
+                        images: [],
+                        summary: '',
+                        bodyContent: '',
+                        bodyContentHTML: "",
+                        addressTitle: '',
+                        sections: [],//array of section id
+                        address: {
+                            lat: "",
+                            lng: "",
+                            aName: ""
+                        },
+                        links: [], //  this will contain action links
+                        backgroundImage: ""
+                    }
+                }
+            }
 
             /**
              * init() private function
@@ -22,6 +56,10 @@
                     };
                 PlaceInfo.get().then(success, error);
             };
+
+            Buildfire.datastore.onUpdate(function (event) {
+                console.log('ON UPDATE called============', event);
+            });
 
             /**
              * init() function invocation to fetch previously saved user's data from datastore.
