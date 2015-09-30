c890e740d91a6353856339ac8f95c0ddda7d6383
schhapola0001@gmail.com <schhapola0001@gmail.com>
2015-09-30 23:31:38 +0530

    code refactored for content.sections.controller.js

--- control/content/app.js
+++ control/content/app.js
@@ -27,54 +27,7 @@
                 .when('/', {
                     templateUrl: 'templates/sections.html',
                     controllerAs: 'ContentSections',
-                    controller: 'ContentSectionsCtrl',
-                    resolve: {
-                        PlaceInfo: ['$q', 'DB', 'COLLECTIONS', 'Orders', 'Location','OrdersItems', function ($q, DB, COLLECTIONS, Orders, Location,OrdersItems) {
-                            var deferred = $q.defer();
-                            var PlaceInfo = new DB(COLLECTIONS.PlaceInfo);
-                            var _bootstrap = function () {
-                                PlaceInfo.save({
-                                    content: {
-                                        images: [],
-                                        descriptionHTML: '',
-                                        description: '',
-                                        sortBy: Orders.ordersMap.Newest,
-                                        rankOfLastItem: '',
-                                        sortByItems: OrdersItems.ordersMap.Newest,
-                                    },
-                                    design: {
-                                        secListLayout: "sec-list-1-1",
-                                        mapLayout: "map-1",
-                                        itemListLayout: "item-list-1",
-                                        itemDetailsLayout: "item-details-1",
-                                        secListBGImage: ""
-                                    },
-                                    settings: {
-                                        defaultView: "list",
-                                        showDistanceIn: "miles"
-                                    }
-                                }).then(function success() {
-                                    Location.goToHome();
-                                }, function fail() {
-                                    _bootstrap();
-                                });
-                            };
-                            PlaceInfo.get().then(function success(result) {
-                                    if (result && result.id && result.data) {
-                                        deferred.resolve(result);
-                                    }
-                                    else {
-                                        //error in bootstrapping
-                                        _bootstrap(); //bootstrap again  _bootstrap();
-                                    }
-                                },
-                                function fail(err) {
-                                    Location.goToHome();
-                                }
-                            );
-                            return deferred.promise;
-                        }]
-                    }
+                    controller: 'ContentSectionsCtrl'
                 })
                 .when('/item/:sectionId', {
                     templateUrl: 'templates/item.html',
