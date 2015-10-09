87d50ce84834073cc2842fed31d3fc92eb1d3197
vineetasharma <vineetasharma18791@gmail.com>
2015-10-09 14:09:35 +0530

    Code Refactored

--- test/control/content/controllers/content.items.spec.controller.js
+++ test/control/content/controllers/content.items.spec.controller.js
@@ -1,11 +1,11 @@
-describe('Unit : Controller - ContentSectionCtrl', function () {
+describe('Unit : Controller - ContentItemsCtrl', function () {
 
 // load the controller's module
     beforeEach(module('placesContent'));
 
     var $q, ContentItems, scope, $routeParams, DB, COLLECTIONS, Modals, Orders, OrdersItems, Messaging, EVENTS, PATHS, Location, placesInfo;
 
-    beforeEach(inject(function (_$q_, _$routeParams_,$controller, _$rootScope_, _DB_, _COLLECTIONS_, _Modals_ ,_Orders_, _OrdersItems_, _Messaging_, _EVENTS_, _PATHS_, _Location_) {
+    beforeEach(inject(function (_$q_, _$routeParams_, $controller, _$rootScope_, _DB_, _COLLECTIONS_, _Modals_, _Orders_, _OrdersItems_, _Messaging_, _EVENTS_, _PATHS_, _Location_) {
             scope = _$rootScope_.$new();
             DB = _DB_;
             COLLECTIONS = _COLLECTIONS_;
@@ -32,7 +32,40 @@ describe('Unit : Controller - ContentSectionCtrl', function () {
                 PATHS: PATHS,
                 Location: Location,
                 Modals: Modals,
-                placesInfo : {}
+                placesInfo: {
+                    data: {
+                        content: {
+                            images: [],
+                            descriptionHTML: '<p>&nbsp;<br></p>',
+                            description: '<p>&nbsp;<br></p>',
+                            sortBy: 'Newest',
+                            rankOfLastItem: '',
+                            sortByItems: 'Newest'
+                        },
+                        design: {
+                            secListLayout: "sec-list-1-1",
+                            mapLayout: "map-1",
+                            itemListLayout: "item-list-1",
+                            itemDetailsLayout: "item-details-1",
+                            secListBGImage: ""
+                        },
+                        settings: {
+                            defaultView: "list",
+                            showDistanceIn: "miles"
+                        }
+                    }
+                },
+                sectionInfo: {
+                    data: {
+                        mainImage: '',
+                        secTitle: '',
+                        secSummary: "This will be summary",
+                        itemListBGImage: '',
+                        sortBy: '',
+                        rankOfLastItem: ''
+                    },
+                    id :'12312412'
+                }
             });
         })
     )
@@ -61,24 +94,28 @@ describe('Unit : Controller - ContentSectionCtrl', function () {
         it('it should pass if PATHS function is defined', function () {
             expect(PATHS).not.toBeUndefined();
         });
+        it('it should pass if PATHS function is defined', function () {
+            expect(PATHS).not.toBeUndefined();
+        });
     });
 
     xdescribe('ContentItems.editSections', function () {
         var Sections;
         beforeEach(inject(function () {
-           /* spy = spyOn(window.Sections, 'find').and.callFake(function () {
-                console.log(786);
-                var deferred = $q.defer();
-                deferred.resolve('Remote call result');
-                return deferred.promise;
-            });*/
+            /* spy = spyOn(window.Sections, 'find').and.callFake(function () {
+             console.log(786);
+             var deferred = $q.defer();
+             deferred.resolve('Remote call result');
+             return deferred.promise;
+             });*/
 
             Sections = jasmine.createSpy().and.callFake(function () {
                 console.log(786);
                 var deferred = $q.defer();
                 deferred.resolve('Remote call result');
                 return deferred.promise;
-            });;
+            });
+            ;
 
         }));
         it('it should pass if ContentItems.editSections calls Items.update', function () {
