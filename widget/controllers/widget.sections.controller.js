f28532bb2f303dd6fd6150737a0c6a5d85536c5d
lakshaydulani <lakshaydulani@gmail.com>
2016-02-12 22:48:47 +0530

    Map 2 issue fix

--- widget/controllers/widget.sections.controller.js
+++ widget/controllers/widget.sections.controller.js
@@ -443,14 +443,17 @@
 
                 /// load items
                 function loadMapCarouselItems(carouselItems) {
+                    $timeout(function () {
+                        if (!mapview || angular.element("#mapCarousel").hasClass('plugin-slider') == false) {
+                            mapview = new Buildfire.components.carousel.view("#mapCarousel", []);  ///create new instance of buildfire carousel viewer
+                        }
 
-                    if (!mapview || angular.element("#mapCarousel").hasClass('plugin-slider') == false) {
-                        mapview = new Buildfire.components.carousel.view("#mapCarousel", []);  ///create new instance of buildfire carousel viewer
-                    }
+                        // create an instance and pass it the items if you don't have items yet just pass []
+                        if (mapview)
+                            mapview.loadItems(carouselItems);
+                    }, 1500);
 
-                    // create an instance and pass it the items if you don't have items yet just pass []
-                    if (mapview)
-                        mapview.loadItems(carouselItems);
+
                 }
 
                 function getItemsDistance(_items) {
@@ -682,8 +685,8 @@
 
                 $scope.$on("Map Carousel:LOADED", function () {
                     /*if (!mapview) {
-                        mapview = new Buildfire.components.carousel.view("#mapCarousel", []);  ///create new instance of buildfire carousel viewer
-                    }*/
+                     mapview = new Buildfire.components.carousel.view("#mapCarousel", []);  ///create new instance of buildfire carousel viewer
+                     }*/
                 });
 
                 if ($routeParams.sectionId) { // this case means the controller is serving the section view
