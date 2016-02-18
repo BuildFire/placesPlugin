b29e187d897ac0981eedc858b0e18ce524a132e0
vineetasharma <vineetasharma18791@gmail.com>
2016-02-18 18:33:06 +0530

    show description condition added

--- widget/controllers/widget.item.controller.js
+++ widget/controllers/widget.item.controller.js
@@ -208,7 +208,7 @@
 
             // Show Body Content when it is not blank
             WidgetItem.showBodyContent = function () {
-                if (WidgetItem.item.data.bodyContent == '<p>&nbsp;<br></p>' || WidgetItem.item.data.bodyContent == '<p><br data-mce-bogus="1"></p>')
+                if (WidgetItem.item.data.bodyContent == '<p>&nbsp;<br></p>' || WidgetItem.item.data.bodyContent == '<p><br data-mce-bogus="1"></p>' || WidgetItem.item.data.bodyContent=='')
                     return false;
                 else
                     return true;
