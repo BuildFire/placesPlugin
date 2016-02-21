04a7ddbf337791ce8ccf54308ab3ae6d886977c2
lakshaydulani <lakshaydulani@gmail.com>
2016-02-21 13:54:19 +0530

    item details map pin fix

--- widget/directives.js
+++ widget/directives.js
@@ -194,7 +194,7 @@
                                 return {
                                     url: _imageUrl,
                                     // This marker is 20 pixels wide by 32 pixels high.
-                                    size: new google.maps.Size(20, 20),
+                                    scaledSize: new google.maps.Size(20, 20),
                                     // The origin for this image is (0, 0).
                                     origin: new google.maps.Point(0, 0),
                                     // The anchor for this image is the base of the flagpole at (0, 32).
