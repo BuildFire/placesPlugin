efefdc19906fb737dcbdbd2f9855f08c6f6d6b22
rishabhdixit <rishabh.dixit@intelligrape.com>
2016-04-07 23:53:09 +0530

    Updated placeholder images in remaining templates

--- control/content/directives.js
+++ control/content/directives.js
@@ -156,5 +156,20 @@
                     };
                 }
             };
+        }])
+        .directive("loadImage", [function () {
+            return {
+                restrict: 'A',
+                link: function (scope, element, attrs) {
+                    element.attr("src", "../../../styles/media/holder-" + attrs.loadImage + ".gif");
+
+                    var elem = $("<img>");
+                    elem[0].onload = function () {
+                        element.attr("src", attrs.finalSrc);
+                        elem.remove();
+                    };
+                    elem.attr("src", attrs.finalSrc);
+                }
+            };
         }]);
 })(window.angular,buildfire);
