0d704e98efe03e71132d43ff152851c294f65768
Vishnu Singh <vish250491@gmail.com>
2016-07-15 18:07:44 +0530

    background-cover applied

--- widget/directives.js
+++ widget/directives.js
@@ -236,7 +236,7 @@
                         }, function (err, imgUrl) {
                             if (imgUrl) {
                                 img = imgUrl;
-                                element.attr("style", 'background:url(' + img + ') !important; background-attachment:scroll !important;');
+                                element.attr("style", 'background:url(' + img + ') !important;background-size: cover !important;');
                             } else {
                                 img = '';
                                 element.attr("style", 'background-color:white');
