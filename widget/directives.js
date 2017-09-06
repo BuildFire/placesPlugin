11929ff1e38c9596f05d581cbe6c27415ccef674
attila buturla <abuturla@buildfire.com>
2017-09-06 15:55:30 -0700

    center anchor for marker image

--- widget/directives.js
+++ widget/directives.js
@@ -68,18 +68,18 @@
                             function getCustomMarkerIcon(_imageUrl) {
                                 return {
                                     url: _imageUrl,
-                                    // This marker is 20 pixels wide by 32 pixels high.
+                                    // This marker is 20 pixels wide by 20 pixels high.
                                     scaledSize: new google.maps.Size(20, 20),
                                     // The origin for this image is (0, 0).
                                     origin: new google.maps.Point(0, 0),
-                                    // The anchor for this image is the base of the flagpole at (0, 32).
-                                    anchor: new google.maps.Point(0, 32)
+                                    // The anchor for this image is at the center of the circle
+                                    anchor: new google.maps.Point(10, 10)
                                 }
                             }
 
                             var selectedLocation = null;
 
-                            var currentLocationIconImageUrl ='https://.app.buildfire.com/app/media/google_marker_blue_icon.png';
+                            var currentLocationIconImageUrl ='https://app.buildfire.com/app/media/google_marker_blue_icon.png';
                             var placeLocationIconImageUrl = 'https://app.buildfire.com/app/media/google_marker_red_icon.png';
                             var selectedLocationIconImageUrl = 'https://app.buildfire.com/app/media/google_marker_green_icon.png';
 
@@ -149,7 +149,7 @@
                             var clusterStyles = [
                                 {
                                     textColor: 'white',
-                                    url: 'http://app.buildfire.com/app/media/google_marker_blue_icon2.png',
+                                    url: 'https://app.buildfire.com/app/media/google_marker_blue_icon2.png',
                                     height: 53,
                                     width: 53
                                 }
@@ -190,7 +190,6 @@
 
                     //scope.firstTimeFilter = true;
                     scope.$watch('filter', function () {
-
                         if (scope.filterUnapplied) {
                             //scope.firstTimeFilter = false;
                             return;
@@ -314,7 +313,7 @@
                                 }
                             }
 
-                            var placeLocationIconImageUrl = 'http://beta.app.buildfire.com/app/media/google_marker_red_icon.png';
+                            var placeLocationIconImageUrl = 'https://app.buildfire.com/app/media/google_marker_red_icon.png';
 
                             var placeLocationIcon = getCustomMarkerIcon(placeLocationIconImageUrl);
 
